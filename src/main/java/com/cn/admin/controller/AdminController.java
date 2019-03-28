db3a4809d688c5f79e91ab5f893dd6dd7a9d3c65
JessionZhang <JessionZhang@SZXFOD150005.gamma.lftltd.net>
2019-03-28 15:16:47 +0800

    update

--- src/main/java/com/cn/admin/controller/AdminController.java
+++ src/main/java/com/cn/admin/controller/AdminController.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.cn.admin.service.AdminService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -18,25 +22,39 @@ import com.cn.admin.service.AdminService;
 @RequestMapping("/admin")
 public class AdminController {
     
-    @Autowired AdminService aService;
+    @Resource
+    private AdminService aService;
     
     @RequestMapping("/index")
-    public String index(String username,String password)
+    public String index(HttpSession session)
     {
-        if(!"".equals(username) && !"".equals(password))
-        {            
+        String login=(String)session.getAttribute("userlogin");
+        if("".equals(login)||login==null)
+        {
+            return "redirect:login";
+        }else{
+            return "admin/manger";  
+        }
+        
+    }
+    
+    @RequestMapping("/login")
+    public String login (HttpServletRequest request,HttpSession session)
+    {
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        if(StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+            return "admin/login";
+        }else{
             boolean result=aService.login(username, password);
             if(result)
             {
-                return "admin/manger";
+                session.setAttribute("userlogin", username);
+                return "redirect:index";
             }else{
-                return "admin/index";
+                return "admin/login";
             }
-            
-        }else{
-            return "admin/index";
         }
-        
     }
     
     
